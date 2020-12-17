3fdba6bcb6f1d10716b4d5b34daa80f67b896dc1
jasons321 <67253740+jasons321@users.noreply.github.com>
2020-12-17 16:08:53 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from time import sleep 
+driver = webdriver.Chrome(executable_path="/Users/jason/Downloads/chromedriver 4")
+driver.get("https://instagram.com")
+sleep(4)
+driver.find_element_by_xpath(("//input[@name=\"username\"]"))\
+    .send_keys(("-"))
+driver.find_element_by_xpath(("//input[@name=\"password\"]"))\
+    .send_keys(("-"))
+driver.find_element_by_xpath(('//button[@type="submit"]'))\
+    .click()
+sleep(3)
+driver.find_element_by_xpath("//button[contains(text(),'Not Now')]")\
+     .click()
+sleep(3)
+driver.find_element_by_xpath("//button[contains(text(),'Not Now')]")\
+     .click()
+sleep(3)
+
+
+for x in range(10):
+    for i in range(3):
+        driver.find_element_by_xpath('//button[text()="Follow"]')\
+            .click()        
+        sleep(2)
+    driver.refresh()
+
+
+
